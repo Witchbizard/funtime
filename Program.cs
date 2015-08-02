dfde53447b3306ac283c207e713f90cd062888c6
Witchbizard <rvalambert@gmail.com>
2015-08-02 01:02:50 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
 using System;
 
 // stuff
+
+//This is broken kinda, but I am too tired to fix it. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CanEdit();
+
+
+        }
+
+        public static void 
+            CanEdit("Can!")
+        {
+            System.Console.WriteLine();
+                                      }
+
+            }
+    }
